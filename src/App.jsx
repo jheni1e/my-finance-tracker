02d90abafc6ca1958fa54a77a004834efdb733a0
Jhenie <highhalma@gmail.com>
2025-09-18 15:12:59 -0300

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
 import IncomeForm from './components/IncomeForm'
+import Home from './pages/Home'
+import About from './pages/About'
+import Finances from './pages/Finances'
 
 function App() {
   return (
     <>
+      <header>
+        <nav className="flex items-center">
+          <Link to="/">Ínicio</Link>
+          <Link to="/finances">Finanças</Link>
+          <Link to="/about">Sobre</Link>
+        </nav>
+      </header>
+
       <IncomeForm></IncomeForm>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/finances" element={<Finances />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Finances.jsx
@@ -0,0 +1,7 @@
+export default function Finances() {
+    return (
+        <>
+            <h1>Visão geral das Finanças</h1>
+        </>
+    );
+}
